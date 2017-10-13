218c560a2067a83384669d962d02fc5ddf8ceeac
Matt Rayner <mattrayner1@gmail.com>
2017-10-13 14:15:42 +0100

    [WEBDEV-343] Update data views to use formats

--- lib/parliament/utils/helpers/format_helper.rb
+++ lib/parliament/utils/helpers/format_helper.rb
@@ -2,22 +2,21 @@ module Parliament
   module Utils
     module Helpers
       module FormatHelper
-        DATA_FORMATS = {
-            'application/n-triples' => 'nt',
-            'text/turtle' => 'ttl',
-            'text/n3' => 'n3',
-            'application/n-quads' => 'nq',
-            'application/x-trig' => 'trig',
-            'application/trix' => 'xml',
-            'application/sparql-results+xml' => 'srx',
-            'application/sparql-results+json' => 'srj',
-            'application/rdf+xml' => 'rdf',
-            'application/json' => 'rj',
-            'text/csv' => 'csv',
-            'text/tab-separated-values' => 'tsv',
-            'text/html' => 'html',
-            'text/vnd.graphviz' => 'gv'
-        }.freeze
+        DATA_FORMATS = %w(
+            application/n-triples
+            text/turtle
+            text/n3
+            application/n-quads
+            application/x-trig
+            application/trix
+            application/sparql-results+xml
+            application/sparql-results+json
+            application/rdf+xml
+            application/json
+            text/csv
+            text/tab-separated-values
+            text/vnd.graphviz
+        ).freeze
         end
       end
     end
