92eaf11a7c755e962d31be0976e8dc96dcd73931
Matt Rayner <m@rayner.io>
2018-05-02 21:16:29 +0100

    Add Request-Id header to parliament header

--- lib/parliament/utils/helpers/parliament_helper.rb
+++ lib/parliament/utils/helpers/parliament_helper.rb
@@ -5,16 +5,19 @@ module Parliament
   module Utils
     module Helpers
       module ParliamentHelper
-        def self.parliament_request
+        def self.parliament_request(request_number: 1)
+          headers = {'Ocp-Apim-Subscription-Key' => ENV['PARLIAMENT_AUTH_TOKEN'] }
+          headers['Request-Id'] = "#{ENV['ApplicationInsights.request.id']}#{request_number}" if ENV['ApplicationInsights.request.id']
+
           Parliament::Request::UrlRequest.new(
-          builder:    Parliament::Builder::NTripleResponseBuilder,
-          headers:    { 'Ocp-Apim-Subscription-Key': ENV['PARLIAMENT_AUTH_TOKEN'] },
-          decorators: Parliament::Grom::Decorator
+            builder:    Parliament::Builder::NTripleResponseBuilder,
+            headers:    headers,
+            decorators: Parliament::Grom::Decorator
           )
         end
 
-        def parliament_request
-          Parliament::Utils::Helpers::ParliamentHelper.parliament_request
+        def parliament_request(request_number: 1)
+          Parliament::Utils::Helpers::ParliamentHelper.parliament_request(request_number)
         end
       end
     end
