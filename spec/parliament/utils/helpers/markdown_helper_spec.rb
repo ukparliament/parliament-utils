e2c141c3d940389dc6abf6a1edf19d82fd52446c
Peter Hunt <naseberry@users.noreply.github.com>
2018-06-18 15:30:26 +0100

    [OUT-256] Fix video embedding so that video starts and ends at specified times

--- spec/parliament/utils/helpers/markdown_helper_spec.rb
+++ spec/parliament/utils/helpers/markdown_helper_spec.rb
@@ -27,9 +27,8 @@ RSpec.describe Parliament::Utils::Helpers::MarkdownHelper do
         it 'converts link to iframe HTML' do
           video_url = 'https://parliamentlive.tv/event/index/1b5736b4-7c93-4827-a02f-abbf0bb36cc8?in=10:00:00&out=12:00:00'
           text = "You can include a video in your markdown #{video_url}"
-          expect(subject.markdown(text)).to eq(
-            "<p>You can include a video in your markdown </p><div class=\"video-wrap\"><iframe src=\"https://videoplayback.parliamentlive.tv/Player/Index/1b5736b4-7c93-4827-a02f-abbf0bb36cc8?in=10:00:00&amp;amp;out=12:00:00&amp;audioOnly=False&amp;autoStart=False&amp;statsEnabled=False\" name=\"UKPPlayer\" title=\"UK Parliament Player\" seamless=\"seamless\" frameborder=\"0\" allowfullscreen style=\"width: 100%;height: 100%;\"></iframe></div>\n"
-          )
+          expected = "<p>You can include a video in your markdown <div class=\"video-wrap\"><iframe src=\"https://videoplayback.parliamentlive.tv/Player/Index/1b5736b4-7c93-4827-a02f-abbf0bb36cc8?in=10:00:00&out=12:00:00&audioOnly=False&autoStart=False&statsEnabled=False\" name=\"UKPPlayer\" title=\"UK Parliament Player\" seamless=\"seamless\" frameborder=\"0\" allowfullscreen style=\"width: 100%; height: 100%;\"></iframe></div></p>\n"
+          expect(subject.markdown(text)).to eq(expected)
         end
       end
     end
