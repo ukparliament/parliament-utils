d54394b41fe4b630c29c5b95072f08f8a7f6c4b2
Matt Rayner <m@rayner.io>
2018-07-03 13:30:15 +0100

    Add new Api-Version header to parliament requests

--- lib/parliament/utils/helpers/parliament_helper.rb
+++ lib/parliament/utils/helpers/parliament_helper.rb
@@ -6,8 +6,11 @@ module Parliament
     module Helpers
       module ParliamentHelper
         def self.parliament_request(request_id = nil, request_number = 1)
-          headers = {'Ocp-Apim-Subscription-Key' => ENV['PARLIAMENT_AUTH_TOKEN'] }
-          headers['Request-Id'] = "#{request_id}#{request_number}" if request_id
+          headers = {}.tap do |headers|
+            headers['Ocp-Apim-Subscription-Key'] = ENV['PARLIAMENT_AUTH_TOKEN']
+            headers['Api-Version'] = ENV['PARLIAMENT_API_VERSION'] if ENV['PARLIAMENT_API_VERSION']
+            headers['Request-Id'] = "#{request_id}#{request_number}" if request_id
+          end
 
           Parliament::Request::UrlRequest.new(
             builder:    Parliament::Builder::NTripleResponseBuilder,
--- lib/parliament/utils/version.rb
+++ lib/parliament/utils/version.rb
@@ -1,5 +1,5 @@
 module Parliament
   module Utils
-    VERSION = '0.8.7'.freeze
+    VERSION = '0.9.0'.freeze
   end
 end
