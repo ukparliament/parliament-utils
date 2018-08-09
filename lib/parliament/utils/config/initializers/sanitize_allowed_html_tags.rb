eb416ee82f683bcdd822bb00835221b3823b1f49
Matt Rayner <m@rayner.io>
2018-08-09 18:15:57 +0100

    Update default sanitisation code to allow colspan attributes

--- lib/parliament/utils/config/initializers/sanitize_allowed_html_tags.rb
+++ lib/parliament/utils/config/initializers/sanitize_allowed_html_tags.rb
@@ -4,9 +4,12 @@
 
 # Don't change this
 default_sanitize_tags = ActionView::Base.sanitized_allowed_tags
+default_sanitize_attributes = ActionView::Base.sanitized_allowed_attributes
 
 # Add your desired HTML tags in here
 desired_sanitize_tags = %w[table tbody td tfoot th thead tr]
+desired_sanitize_attributes = %w[colspan]
 
 # Merge default_sanitize_tags with desired_sanitize_tags
 Rails.application.config.action_view.sanitized_allowed_tags = default_sanitize_tags.merge(desired_sanitize_tags).to_a
+Rails.application.config.action_view.sanitized_allowed_attributes = default_sanitize_attributes.merge(desired_sanitize_attributes).to_a
--- lib/parliament/utils/version.rb
+++ lib/parliament/utils/version.rb
@@ -1,5 +1,5 @@
 module Parliament
   module Utils
-    VERSION = '0.10.2'.freeze
+    VERSION = '0.11.0'.freeze
   end
 end
